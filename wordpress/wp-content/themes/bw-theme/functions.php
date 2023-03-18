5fe8c02c3680f9a8718b646ea83662e1cb7bd681
Eduard Kolesnikov <eduardkolesnikov@mail.ru>
2023-03-18 12:30:27 +0300

    Перенёс шрифты из style в fontello

--- wordpress/wp-content/themes/bw-theme/functions.php
+++ wordpress/wp-content/themes/bw-theme/functions.php
@@ -6,6 +6,9 @@ function add_scripts_and_styles() {
     wp_register_script( 'jquery', get_template_directory_uri() . '/assets/js/jquery-3.6.3.min.js', false, null, true);
     wp_enqueue_script( 'jquery' );
     wp_enqueue_script( 'main', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), null, 'footer');
-    wp_enqueue_style('main', get_stylesheet_uri());
+
+    wp_enqueue_style('fontello', get_template_directory_uri(  ) . '/assets/css/fontello.css');
+
+    wp_enqueue_style('main', get_stylesheet_uri(), array('fontello'));
 } 
 ?>
